d2062d09bddc79c4c5d21ac793f0141e316f9dd2
Dili Wu <wudili@Dili-Wus-MacBook-Pro-Retina-2.local>
2015-05-20 11:34:10 -0700

    Create maven

---
+++ src/main/java/org/silu/admission/school_rank/App.java
@@ -0,0 +1,13 @@
+package org.silu.admission.school_rank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
